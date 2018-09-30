3b52363407e56c470adf2482227a90e1eb98b550
Chao Feng <chaofeng@Chaos-MacBook-Pro.local>
2018-09-30 10:03:01 -0700

    Add one question. 20180930

--- LeeC/001_TwoSum/Solution.java
+++ LeeC/001_TwoSum/Solution.java
@@ -1,10 +1,16 @@
 
+import java.util.*;
 
 
 class Solution {
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSumBF(int[] nums, int target) {
         
+        for(int i=0; i<nums.length; i++)
+            System.out.print(nums[i] + "  ");
+        System.out.println();
+
+        System.out.println("target = " + target); 
         int[] index = new int[2];
         
         for(int i=0; i<nums.length-1; i++){
@@ -13,6 +19,11 @@ class Solution {
                 
                 if(nums[i]+nums[j] == target){
                     index[0] = i; index[1] = j;
+                   
+                    for(int x=0; x<index.length; x++)
+                        System.out.print(index[x] + "  ");
+                    System.out.println(); 
+                    
                     return index;
                 }
             }
@@ -23,7 +34,70 @@ class Solution {
         
     }
 
-    public static void main(String argv[]){
+    public int[] twoSumTwoPass(int[] nums, int target) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        System.out.println("map = " + map);                
+        System.out.println("map.size() = " + map.size()); 
 
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSumOnePass(int[] nums, int target) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+    
+        for (int i = 0; i < nums.length; i++) {
+            
+           int complement = target - nums[i];
+           
+           if (map.containsKey(complement)) {
+              
+              int[] index = new int[2];
+              index[0] = map.get(complement);
+              index[1] = i;
+
+              return index;
+            }
+
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    } 
+
+    public static void main(String argv[]){
+        Solution sol = new Solution();
+
+        int[] nums = new int[]{2, 7, 11, 15};  
+        int target = 9;
+        sol.twoSumBF(nums, target);
+          
+        /* Only one solution.
+        nums = new int[]{2,2,2,2};
+        int target = 4;
+        sol.twoSumBF(nums, target);
+        */
+        
+        sol.twoSumTwoPass(nums, target);
+
+        nums = new int[]{2,2,2,2};
+        target = 4;
+        sol.twoSumTwoPass(nums, target);
+
+        nums = new int[]{4,4,5,8};
+        target = 8;
+        sol.twoSumTwoPass(nums, target);
+ 
+               
     }
 }
