a4a2aa60a9d3f5de98ea9f60a871ec632921053a
Chao Feng <fengchaokaoyan@gmail.com>
2019-08-14 14:05:57 -0700

    Delete pictures. Write into code.

--- Interviews/20180917_me_FB_Phone/merge.java
+++ Interviews/20180917_me_FB_Phone/merge.java
@@ -1,16 +1,23 @@
-//Question:
-//Input:
-//arr1 = {[1,2], [3,9]}
-//arr2 = {[4,6], [8,10], [11,12]}
-//
-//Output = {[1,2], [3,10], [11,12]}
-//
-//merge(arr1, arr2) -> arr3
+/*
+Intervier: ET(White Bearded Muslim)
+
+Question:
+
+Input:
+arr1 = {[1,2], [3,9]}
+
+arr2 = {[4,6], [8,10], [11,12]}
+
+Output = {[1,2], [3,10], [11,12]}
+
+Implement the function: merge(arr1, arr2) -> arr3
+*/
 
 import java.util.*;
 
-class Merge{
-public int[][] merge(int[][] arr1, int[][] arr2){
+public class Merge{
+
+    public int[][] merge(int[][] arr1, int[][] arr2){
         for(int i=0; i<arr1.length; i++){
             for(int j=0; j<arr1[0].length; j++){
                 System.out.print(arr1[i][j] + "  ");
@@ -23,32 +30,30 @@ public int[][] merge(int[][] arr1, int[][] arr2){
             }
         }
         System.out.println("");
-    int[] start = new int[arr1.length+arr2.length];
-    int[] end = new int[arr1.length+arr2.length];
-    for(int i=0; i < arr1.length; i++){
-        start[i] = arr1[i][0];
-        end[i] = arr1[i][1];
-    }
-    for(int i=0; i< arr2.length; i++){
-        start[i+arr1.length] = arr2[i][0];
-        end[i+arr1.length] = arr2[i][1];
-    }
-    Arrays.sort(start);
-    Arrays.sort(end);
-    int startIndex = 0, endIndex = 0;
-    List<int[]> re = new ArrayList<>();
-    while( endIndex < start.length){
-        if(endIndex+1 == start.length || start[endIndex+1] > end[endIndex]){
-            re.add(new int[]{start[startIndex], end[endIndex]});
-            startIndex = endIndex+1;
+        int[] start = new int[arr1.length+arr2.length];
+        int[] end = new int[arr1.length+arr2.length];
+        for(int i=0; i < arr1.length; i++){
+            start[i] = arr1[i][0];
+            end[i] = arr1[i][1];
         }
-	endIndex++;
+        for(int i=0; i< arr2.length; i++){
+            start[i+arr1.length] = arr2[i][0];
+            end[i+arr1.length] = arr2[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int startIndex = 0, endIndex = 0;
+        List<int[]> re = new ArrayList<>();
+        while( endIndex < start.length){
+            if(endIndex+1 == start.length || start[endIndex+1] > end[endIndex]){
+                re.add(new int[]{start[startIndex], end[endIndex]});
+                startIndex = endIndex+1;
+            }
+	    endIndex++;
+        }
+        return re.toArray(new int[re.size()][]);
     }
-    return re.toArray(new int[re.size()][]);
-}
-}
 
-class Solution{
     public static void main(String args[]){
         Merge m = new Merge();
         int[][] arr1 = {{1,2}, {3,9}};
@@ -63,6 +68,3 @@ class Solution{
         System.out.println("");
     }
 }
-
-
- 
---
+++ Interviews/20180926_me_BL_Phone/Solution.java
@@ -0,0 +1,34 @@
+
+
+/*
+Interviwer: Indian Man (Name Unknown)
+
+
+Question:
+
+Input
+---------
+a = [1,5,6,-2,6]
+b = [6,6,6]
+
+Output
+---------
+c = [-2, 6, 6] or [6, -2, 6] or [6, 6, -2]
+
+
+
+*/
+
+
+public class Solution{
+
+    public int[] intersection(int[] a, int[] b){
+        
+    }
+
+
+    public static void main(String args[]) throws Exception {
+
+    }
+
+}
